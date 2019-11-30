5f1ad6c44d1cfd732e21cedee9b65b18d5df93b1
Aashrut Vaghani <49032425+Aashrut@users.noreply.github.com>
2019-11-30 18:02:41 +0530

    Update PROG14.C

--- PROG14.C
+++ PROG14.C
@@ -3,17 +3,17 @@
 #include<ctype.h>
 int main()
 {
-char c;
-printf("This Program will check that given character is capital or small or digit or any special character.\n");
-printf("Enter the character : \n");
-scanf("%c",&c);
-if(isupper(c))
-printf("The given character is Capital.");
-else if(islower(c))
-printf("The given character is Smaller.");
-else if(isdigit(c))
-printf("The given character is Digit.");
-else
-printf("The given number is Special Character.");
-return 0;
+  char c;
+  printf("This Program will check that given character is capital or small or digit or any special character.\n");
+  printf("Enter the character : \n");
+  scanf("%c",&c);
+  if(isupper(c))
+    printf("The given character is Capital.");
+  else if(islower(c))
+    printf("The given character is Smaller.");
+  else if(isdigit(c))
+    printf("The given character is Digit.");
+  else
+    printf("The given number is Special Character.");
+  return 0;
 }
