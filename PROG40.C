85b75d905aa51f67cd66427234a62c841c1f8cb6
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-11-30 21:15:12 +0530

    Create PROG40.C

---
+++ PROG40.C
@@ -0,0 +1,25 @@
+//40. Write a program to print patterns as given below:
+// * * * * * * * *
+// * * * * * * *
+// * * * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+#include<stdio.h>
+int main()
+{
+  int i,j,n;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=n;i>=1;i--)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("* ");
+    }
+    printf("\n");
+  }
+}
