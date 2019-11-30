ab1dcb118b55e4139b64abc260b184c787e4f7c6
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-11-30 17:33:35 +0530

    Create PROG7.C

---
+++ PROG7.C
@@ -0,0 +1,17 @@
+//4. Write a C program to interchange two numbers.
+#include<stdio.h>
+int main()
+{
+	int  a,b,x;
+	printf("This program interchanges the given two numbers.\n");
+	printf("Enter the First Number a: \n");
+	scanf("%d",&a);
+	printf("Enter the Second Number b: \n");
+	scanf("%d",&b);
+	printf("Before Interchange, a=%d, b=%d\n",a,b);
+	x=a;
+  a=b;
+  b=x;
+  printf("After Interchange, a=%d, b=%d",a,b);
+  return 0;
+}
