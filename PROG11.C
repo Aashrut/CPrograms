64d5300dcc93d02f9f3ca4eaa099ea3e03214ef1
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-11-30 17:47:45 +0530

    Create PROG11.C

---
+++ PROG11.C
@@ -0,0 +1,16 @@
+//11. Write a C program to find that the accepted number is Negative, or Positive or Zero.
+#include<stdio.h>
+int main()
+{
+	float a;
+	printf("This Program finds that the given number is Negative or Positive or Zero.\n");
+	printf("Enter the Number : \n");
+	scanf("%f",&a);
+	if (a<0)
+	    printf("The Number is Negative.");
+	else if (a==0)
+	    printf("The Number is Zero.");
+	else if (a>0)
+	    printf("The Number is Positive.");
+  return 0;
+}
