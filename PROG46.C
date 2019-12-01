c3045d854c8171be2745af2896bcc56c3f397b4f
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-12-01 22:14:44 +0530

    Create PROG46.C

---
+++ PROG46.C
@@ -0,0 +1,31 @@
+//46. Write a program to print patterns(Pascal's triangle) as given below:
+//          1
+//         1 1
+//        1 2 1
+//       1 3 3 1
+//      1 4 6 4 1
+
+#include<stdio.h>
+int main()
+{
+  int i,j,k,n,temp=1;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=0;i<=n-1;i++)
+  {
+    for(k=n-i;k>=1;k--)
+    {
+      printf(" ");
+    }
+    for(j=0;j<=i;j++)
+    {
+      if(j==0 || i==0)
+      temp=1;
+      else
+      temp=temp*(i-j+1)/j;
+      printf("%d ",temp);
+    }
+    printf("\n");
+  }
+  return 0;
+} 
