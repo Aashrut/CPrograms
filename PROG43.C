87b4fb185972af984cf68cb174bd1233bb21d2df
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-12-01 21:55:00 +0530

    Create PROG43.C

---
+++ PROG43.C
@@ -0,0 +1,24 @@
+//43. Write a program to print patterns as given below:
+// 666666
+// 55555
+// 4444
+// 333
+// 22
+// 1
+
+#include<stdio.h>
+int main()
+{
+  int i,j,k,n;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=n;i>=1;i--)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("%d",i);
+    }
+    printf("\n");
+  }
+  return 0;
+}
