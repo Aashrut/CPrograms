58b098bd365aa3c77334e30be132203db7a8c7cb
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-12-01 21:47:35 +0530

    Create PROG42.C

---
+++ PROG42.C
@@ -0,0 +1,23 @@
+//41. Write a program to print patterns as given below:
+// 123456
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+#include<stdio.h>
+int main()
+{
+  int i,j,k,n;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=n;i>=1;i--)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("%d",j);
+    }
+    printf("\n");
+  }
+}
