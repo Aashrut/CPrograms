4490e0d4f052b24a99fc1799e874cbbdb98e6f9f
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-12-05 20:58:07 +0530

    Create PROG48.C

---
+++ PROG48.C
@@ -0,0 +1,33 @@
+//48. Write a program to print patterns as given below:
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+#include<stdio.h>
+int main()
+{
+  int i,j,n;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("%d ",j);
+    }
+    printf("\n");
+  }
+  for(i=n-1;i>=1;i--)
+  {
+    for(j=1;j<=i;j++)
+    {
+      printf("%d ",j);
+    }
+    printf("\n");
+  }
+  return 0;
+}
