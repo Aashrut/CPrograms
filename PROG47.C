c936a60c745b7f7a45b12a395233d204dcd22a45
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-12-05 20:46:18 +0530

    Create PROG47.C

---
+++ PROG47.C
@@ -0,0 +1,30 @@
+//47. Write a program to print patterns as given below:
+//         *
+//       * # *
+//     * # # # *
+//   * # # # # # *
+//  * # # # # # # *
+
+#include<stdio.h>
+int main()
+{
+  int i,j,k,n;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(k=n-i;k>=1;k--)
+    {
+      printf("  ");
+    }
+    for(j=1;j<=2*i-1;j++)
+    {
+      if(j==1 || j==(2*i-1))
+      printf("* ");
+      else
+      printf("# ");
+    }
+    printf("\n");
+  }
+  return 0;
+}
