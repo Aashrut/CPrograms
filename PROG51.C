8f64ebf86ad0a127ab8aedea6e70203791fac045
Aashrut Vaghani <Aashrut@users.noreply.github.com>
2019-12-13 21:06:14 +0530

    Create PROG51.C

---
+++ PROG51.C
@@ -0,0 +1,19 @@
+// 51. 39. Write a C program to read and store the roll no and marks of 20 students using array.
+
+#include<stdio.h>
+int main()
+{
+    int m[20][2],i;
+    for(i=0;i<20;i++)
+    {
+        printf("Enter the Roll number and marks : ");
+        scanf("%d %d",&m[i][0],&m[i][1]);
+    }
+    printf("The given data are : \n");
+    printf("Roll No. Marks\n");
+    for(i=0;i<20;i++)
+    {
+        printf("%d        %d\n",m[i][0],m[i][1]);
+    }
+    return 0;
+}
